536dda25cb84015b0727c996dd379289e5144a78
arihito <arihito.m@gmail.com>
2017-01-29 16:20:48 +0900

    get json datas

--- app.js
+++ app.js
@@ -7,6 +7,29 @@ fetch(url).then(function (data) {
 
 }).then(function(json) {
 
-  console.log(json.feed.entry[0].category.attributes.label);
+  var jsonEntry = json.feed.entry;
+
+  // 取得した情報分だけ繰り返して表示
+  for(var i=0; i<jsonEntry.length; i++) {
+
+    // 画像URLをオブジェクトから取得
+    var imgUrl = jsonEntry[i][img:image][0].label;
+
+    // アプリタイトルを取得
+    var appTitle = jsonEntry[i].title.label;
+
+    // アプリのダウンロードURLを取得
+    var appLink = jsonEntry[i].link[0].attributes.href;
+
+    // 販売価格を取得
+    var appPrice = jsonEntry[i].[im:price].attributes.label;
+
+    // 詳細説明を取得
+    var appSummary = jsonEntry[i].summary.label;
+
+    // 著作権を取得
+    var appCredit = jsonEntry[i].rights.label;
+
+  }
 
 });
